035740d87458623479eeb65bdb9d0d019f64765d
Richard Melchior <raustin.melchior@gmail.com>
2018-06-05 00:10:35 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,6 +3,10 @@ class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
 
 
+  def initialize(
+    
+  end
+
   def cobble
     puts "Your shoe is as good as new!"
     @condition = "new"
